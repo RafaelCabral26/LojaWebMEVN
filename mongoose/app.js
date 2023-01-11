5c5f0c8595be30dc0fc0c1a08884b58a41abbdbd
Rafael Cabral <rafaelcabral261992@gmail.com>
2023-01-11 18:23:13 -0300

    teste .env mongo

--- mongoose/app.js
+++ mongoose/app.js
@@ -3,13 +3,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const bodyParser = require("body-parser");
 
 const corsOption = {
     origin: "http://localhost:3000"
 }
 app.use(cors());
-app.use(bodyParser.json())
+app.use(express.json())
 
 
 const conn = require("../db/connmong");
@@ -23,7 +22,7 @@ app.use("/api", routes);
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.MONGODB_URL || 3000;
 
 app.listen(PORT,function() {
     console.log(`Server rodando na porta: ${PORT}`)
